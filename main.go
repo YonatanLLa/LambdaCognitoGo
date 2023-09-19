9d9f0f8d0a157fb7f945e4e45ede35790d8e20c6
yonatanlla <yonatanllanto174@gmail.com>
2023-09-19 17:06:29 -0500

    validations lambda

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	lambda "github.com/aws/aws-lambda-go/lambda"
 	"github.com/yonatan/gambituser/awsgo"
+	"github.com/yonatan/gambituser/models"
+
 )
 
 func main() {
@@ -15,4 +20,18 @@ func main() {
 
 func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
 	awsgo.InicializoAWS()
+
+	if !ValidoParametros() {
+		fmt.Println("Error en los parametros. debe enviar 'SecretName'")
+		err := errors.New("error en los parametros debe enviar SecreteName")
+		return event, err
+	}
+
+}
+
+func ValidoParametros() bool {
+	var traeParametro bool
+	_, traeParametro = os.LookupEnv("SecretName")
+
+	return traeParametro
 }
